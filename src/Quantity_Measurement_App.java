fc59a3afd4436d6951bd978bcb1dd73805c887b1
kanishkupadhyay1 <kanishk.upadhyay2006@gmail.com>
2026-04-17 09:41:21 +0530

    UC1: Extended Unit Support

--- src/Quantity_Measurement_App.java
+++ src/Quantity_Measurement_App.java
@@ -3,7 +3,9 @@ public class Quantity_Measurement_App {
 
     enum LengthUnit {
         FEET(1.0),
-        INCH(1.0 / 12.0);
+        INCH(1.0 / 12.0),
+        YARDS(3.0),
+        CENTIMETERS(0.0328084); // 1 cm = 0.0328084 feet
 
         private final double toFeetFactor;
 
@@ -47,18 +49,25 @@ public class Quantity_Measurement_App {
 
     public static void main(String[] args) {
 
-        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+        // Yard to Feet
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.YARDS);
+        QuantityLength q2 = new QuantityLength(3.0, LengthUnit.FEET);
+        System.out.println("Yard to Feet: " + q1.equals(q2));
 
-        System.out.println("Feet to Inches: " +
-                (q1.equals(q2) ? "Equal (true)" : "Not Equal (false)"));
+        // Yard to Inches
+        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.YARDS);
+        QuantityLength q4 = new QuantityLength(36.0, LengthUnit.INCH);
+        System.out.println("Yard to Inches: " + q3.equals(q4));
 
-        // Inch vs Inch
-        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.INCH);
-        QuantityLength q4 = new QuantityLength(1.0, LengthUnit.INCH);
+        // CM to Inches
+        QuantityLength q5 = new QuantityLength(1.0, LengthUnit.CENTIMETERS);
+        QuantityLength q6 = new QuantityLength(0.393701, LengthUnit.INCH);
+        System.out.println("CM to Inches: " + q5.equals(q6));
 
-        System.out.println("Inch to Inch: " +
-                (q3.equals(q4) ? "Equal (true)" : "Not Equal (false)"));
+        // Same unit check
+        QuantityLength q7 = new QuantityLength(2.0, LengthUnit.YARDS);
+        QuantityLength q8 = new QuantityLength(2.0, LengthUnit.YARDS);
+        System.out.println("Yard same: " + q7.equals(q8));
     }
     }
 

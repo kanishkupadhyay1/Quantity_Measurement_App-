ab863b7207e6af927f1b28b9c945a201ed5ebcd2
samarthh302 <samarthverma2890@gmail.com>
2026-04-17 09:26:29 +0530

    UC1UC1: Feet measurement equality

--- src/Quantity_Measurement_App.java
+++ src/Quantity_Measurement_App.java
@@ -1,5 +1,54 @@
+import java.util.*;
 public class Quantity_Measurement_App {
-    public static void main(String[] args){
-        System.out.println("initialization...");
+
+    static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            Feet other = (Feet) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter first value in feet: ");
+            double val1 = scanner.nextDouble();
+
+            System.out.print("Enter second value in feet: ");
+            double val2 = scanner.nextDouble();
+
+            Feet feet1 = new Feet(val1);
+            Feet feet2 = new Feet(val2);
+
+            boolean result = feet1.equals(feet2);
+
+            if (result) {
+                System.out.println("Equal (true)");
+            } else {
+                System.out.println("Not Equal (false)");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Invalid input! Please enter numeric values only.");
+        } finally {
+            scanner.close();
+        }
     }
 }
